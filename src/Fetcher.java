d6a9a8feeb17d1de2aae357b842380fdd2e678ae
Akvile1206 <akvile.val1206@gmail.com>
2019-05-16 15:23:35 +0100

    progressing search

--- src/Fetcher.java
+++ src/Fetcher.java
@@ -1,8 +1,15 @@
-import java.util.*;
-import java.io.*;
-import java.net.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /* TODO:
