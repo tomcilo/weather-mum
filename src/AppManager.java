c2b6cbf6b24ed0d84de3bb580aec9678775b9379
Maja Trela <maja.ada0@gmail.com>
2019-05-20 17:23:58 +0100

    Dummy AppManager

--- src/AppManager.java
+++ src/AppManager.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AppManager {
 
@@ -10,36 +13,44 @@ public class AppManager {
     private MainScreen mainScreen;
     private JFrame mainFrame;
 
-    public AppManager() {
+    public AppManager() throws IOException {
         mainFrame = new JFrame();
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainFrame.setSize(StyleGuide.getScreenWidth(), StyleGuide.getScreenHeight());
+        mainFrame.setSize(2*StyleGuide.getScreenWidth(), StyleGuide.getScreenHeight());
 
-        //mainFrame.setVisible(true);
+        mainFrame.setLayout(new GridLayout(1, 3));
+
+
+        Map<String, String> f = new HashMap<>();
+        f.put("Temperature", "17.8");
+        f.put("WeatherText", "Cloudy");
+        f.put("WeatherIcon", "7");
+        Weather w = new Weather(
+                Recommender.recommend(f),
+                "Monday", new Date(), 10, f.get("WeatherText"),
+                Float.parseFloat(f.get("Temperature")), 20, 10,
+                Float.parseFloat(f.get("Temperature")));
+        ArrayList we = new ArrayList<Weather>();
+        we.add(w);
+        City c = new City("Test", we, we, w, 20, 10);
+
 
         cityListScreen = new CityListScreen();
+        mainFrame.add(cityListScreen);
         searchScreen = new SearchScreen();
+        mainFrame.add(searchScreen);
+        //mainScreen = new MainScreen(c);
+        //mainFrame.add(mainScreen);
+        mainFrame.setVisible(true);
     }
 
-
-    public void setCityListScreenVisible() {
-        //mainFrame.add(cityListScreen);
-        //mainFrame.setVisible(true);
-        cityListScreen.setVisible(true);
+    //This is a very generic thing and it's bad but we'll improve on that
+    public void notify(String message) {
+        System.out.println(message);
     }
 
     public static void main(String[] args) throws IOException {
         AppManager appManager = new AppManager();
-        appManager.setCityListScreenVisible();
 
-        /*
-        Weather w = new Weather(StyleGuide.getSunnyIcon(), StyleGuide.getSunnyIcon(),
-                new Clothing(StyleGuide.getUmbrellaIcon(), StyleGuide.gettShirtIcon(),
-                        StyleGuide.getShortShortsIcon(), StyleGuide.getSearchIcon()),
-                "Monday", new Date(), 10, "Sunny", 18, 20, 10, 20);
-        ArrayList we = new ArrayList<Weather>();
-        we.add(w);
-        appManager.createMainScreen(new City("Test", we, we, w, 20, 10));
-        */
     }
 }
